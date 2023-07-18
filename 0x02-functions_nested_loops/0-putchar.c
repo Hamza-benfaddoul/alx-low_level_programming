a1ec3460957c654b7d0f0fbb2e678c7f757de367
Hamza-benfaddoul <benfaddoul01@gmail.com>
2023-07-18 10:18:55 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: 0
+ */
+int	main(void)
+{
+		char *str = "_putchar";
+
+		while (*str)
+		{
+			_putchar(str++);
+		}
+
+		_putchar("\n");
+		return (0);
+}
