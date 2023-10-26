5196c475f09108389d7888de8bd68d8dde03ce99
Hamza Benfaddoul <hbenfadd@e1r3p11.1337.ma>
2023-10-26 16:52:18 +0100

    Task 1 updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,27 +8,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int num_bits;
-	int i;
-	unsigned long int mask;
-	int flag;
+	unsigned long int temp;
+	int shifts;
 
-	flag = 0;
-	num_bits = 8 * sizeof(unsigned long int);
-	i = num_bits;
-	while (i - 1 >= 0)
+	if (n == 0)
 	{
-		mask = 1UL << i;
-		if (n & mask)
-		{
-			_putchar("1");
-			flag = 1;
-		}
-		else if (flag)
-			_putchar("0");
-		i--;
-	}
-	if (!flag)
 		_putchar("0");
-	_putchar("\n");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			_putchar("1");
+		else
+			_putchar("0");
+	}
 }
